e7d7dc8f384f55e527e18456e6ae3651f4a8ae78
guilhermerodriguesmusic <guilhermerodriguesmusic@gmail.com>
2026-09-19 09:30:17 +0100

    Refresh app cache for provider onboarding update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='triangulo-pilot-v9';
+const CACHE='triangulo-pilot-v10';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./logo.svg']))) });
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const clone=r.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))))});
