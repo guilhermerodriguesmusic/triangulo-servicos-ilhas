8584154a923d76cfeedfdc3807d3e0576cb2d8f8
guilhermerodriguesmusic <guilhermerodriguesmusic@gmail.com>
2026-09-19 12:38:07 +0100

    Refresh cache for APK download flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='triangulo-pilot-v19';
+const CACHE='triangulo-pilot-v20';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./app.html','./instalar/','./manifest.json','./logo.svg']))) });
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const clone=r.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))))});
