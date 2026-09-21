8745d7b5877461de8b963fdcbbd05c18ce55eb8f
guilhermerodriguesmusic <guilhermerodriguesmusic@gmail.com>
2026-09-21 22:33:21 +0100

    Refresh cache for current v2 app icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='triangulo-pwa-20260921-v1-0-rc32';
+const CACHE='triangulo-pwa-20260921-v1-0-rc33';
 const CORE=[
   '/',
   '/index.html',
@@ -18,10 +18,10 @@ const CORE=[
   '/assets/pico-real.jpg',
   '/assets/faial-real.jpg',
   '/assets/sao-jorge-real.jpg',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/icons/icon-512-maskable.png',
-  '/apple-touch-icon.png'
+  '/icon-192-v2.png',
+  '/icon-512-v2.png',
+  '/icons/icon-512-maskable-v2.png',
+  '/apple-touch-icon-v2.png'
 ];
 
 const PRIVATE_ENTRY_PARAMS=[
